0282d2700572e501da5d3c466c1d1bdb9d55611a
nihat deniz özkan <136701651+bissha2@users.noreply.github.com>
2023-10-21 01:00:19 +0300

    Update main.cpp
    array function added

--- main.cpp
+++ main.cpp
@@ -14,19 +14,38 @@ int main()
     
     ifstream readonly("blobs1.txt", ios::in);
     readonly >> a;
+
     readonly >> b;
+
+    
+
     arr.resize(a);
     for (int i = 0; i < a; i++) {
         arr[i].resize(b);
     }
     for (int c = 0; c < a; c++) {
+       
         for (int d = 0; d < b; d++) {
-            readonly  >> noskipws >> arr[c][d];
+            readonly  >> noskipws >>arr[c][d];
+            if (arr[c][d] == 10) {
+                arr[c][d] = NULL;
+                --d;
+              
+            }
         }
+        
     }
+
     for (int c = 0; c < a; c++) {
+        
         for (int d = 0; d < b; d++) {
+
             cout << arr[c][d];
+
         }
+        cout << endl;
     }
+    
+
+
 }
