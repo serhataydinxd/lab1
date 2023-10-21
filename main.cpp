37a969a975831fed163e40ad2d51b9adcaa2676a
serhataydinxd <serhat4aydin@gmail.com>
2023-10-21 11:14:48 +0300

    beautified

--- main.cpp
+++ main.cpp
@@ -9,39 +9,27 @@ vector<vector<char>>arr;
 
 
 
-int main()
-{
-    
+int main(void){
     ifstream readonly("blobs1.txt", ios::in);
-    readonly >> a;
-
-    readonly >> b;
-
-    
-
+    readonly >> a >> b;
     arr.resize(a);
     for (int i = 0; i < a; i++) {
         arr[i].resize(b);
     }
     for (int c = 0; c < a; c++) {
-       
         for (int d = 0; d < b; d++) {
             readonly  >> noskipws >>arr[c][d];
             if (arr[c][d] == 10) {
                 arr[c][d] = NULL;
                 --d;
-              
             }
         }
-        
     }
 
     for (int c = 0; c < a; c++) {
         
         for (int d = 0; d < b; d++) {
-
             cout << arr[c][d];
-
         }
         cout << endl;
     }
