6d9debb09e7703cedb4381a35acc5883eb5576c5
serhataydinxd <serhat4aydin@gmail.com>
2023-10-21 20:42:35 +0300

    Update

--- main.cpp
+++ main.cpp
@@ -5,10 +5,15 @@
 
 void readFile(void);
 void print(void);
+void findBlobs(void);
 
 using namespace std;
-int a, b;
+int row, column;
+string fileName;
 vector<vector<char>>arr;
+vector<vector<char>>arrYedek;
+vector <int> blobRow;
+vector <int> blobColumn;
 
 int main(void){
     readFile();
@@ -17,14 +22,16 @@ int main(void){
 }
 
 void readFile (void){
-    ifstream readonly("blobs1.txt", ios::in);
-    readonly >> a >> b;
-    arr.resize(a);
-    for (int i = 0; i < a; i++) {
-        arr[i].resize(b);
+    cout << "Enter the file name: ";
+    cin >> fileName;
+    ifstream readonly(fileName, ios::in);
+    readonly >> row >> column;
+    arr.resize(row);
+    for (int i = 0; i < row; i++) {
+        arr[i].resize(column);
     }
-    for (int c = 0; c < a; c++) {
-        for (int d = 0; d < b; d++) {
+    for (int c = 0; c < row; c++) {
+        for (int d = 0; d < column; d++) {
             readonly  >> noskipws >>arr[c][d];
             if (arr[c][d] == 10) {
                 arr[c][d] = NULL;
@@ -35,10 +42,48 @@ void readFile (void){
 }
 
 void print(void){
-    for (int c = 0; c < a; c++) {
-        for (int d = 0; d < b; d++) {
+    for (int c = 0; c < row; c++) {
+        cout << "  ";
+        for (int d = 0; d < column; d++) {
+            cout << d%10;
+        }
+        cout << endl << " +";
+        for (int d = 0; d < column; d++) {
+            cout << "-";
+        }
+        cout << "+" << endl;
+        break;
+    }
+    for (int c = 0; c < row; c++) {
+        cout << c%10 << "|";
+        for (int d = 0; d < column; d++) {
             cout << arr[c][d];
         }
+        cout << "|" << c%10 << endl;
+    }
+    for (int c = 0; c < row; c++) {
+        cout << " +";
+        for (int d = 0; d < column; d++) {
+            cout << "-";
+        }
+        cout << "+" << endl;
+        cout << "  ";
+        for (int d = 0; d < column; d++) {
+            cout << d%10;
+        }
         cout << endl;
+        break;
+    }
+}
+
+
+void findBlobs(void){
+    for (int c = 0; c < row; c++) {
+        for (int d = 0; d < column; d++) {
+            if (arr[c][d] == 'X') {
+                blobRow.push_back(c);
+                blobColumn.push_back(d);
+            }
+        }
     }
 }
