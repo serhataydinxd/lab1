025d0f665056fa16fd4f504928ab80a76d652604
serhataydinxd <serhat4aydin@gmail.com>
2023-11-13 16:11:25 +0300

    lab03

---
+++ Lab03/ExpressionEvaluator/Expression.cpp
@@ -0,0 +1,20 @@
+#include <string>
+
+using namespace std;
+
+///----------------------------------------------------------------------------------
+/// Given an expression in infix-notation, converts it to a string in post-fix notation 
+/// 
+string Infix2Postfix(string &s) {
+	// Fill this in
+	string result;
+
+	return result;
+} // end-Infix2Postfix
+
+///----------------------------------------------------------------------------------
+/// Given a string in post-fix notation, evaluates it and returns the result
+/// 
+int EvaluatePostfixExpression(string& s) {
+	return 0;
+} // end-EvaluatePostfixExpression
