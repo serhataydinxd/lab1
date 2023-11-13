6be7de394f3bdf810e2f4a6c58848be0da71c4bd
nihat deniz özkan <136701651+bissha2@users.noreply.github.com>
2023-11-13 16:53:37 +0300

    commit
    commit

---
+++ Expression.cpp
@@ -0,0 +1,73 @@
+#include <string>
+#include <stack>
+using namespace std;
+int prec(char c)
+{
+    if (c == '^')
+        return 3;
+    else if (c == '/' || c == '*')
+        return 2;
+    else if (c == '+' || c == '-')
+        return 1;
+    else
+        return -1;
+}
+///----------------------------------------------------------------------------------
+/// Given an expression in infix-notation, converts it to a string in post-fix notation 
+/// 
+string Infix2Postfix(string &s) {
+
+    stack<char> st;
+	string result;
+    for (int i = 0; i < s.length(); i++) {
+        char c = s[i];
+
+        // If the scanned character is
+        // an operand, add it to output string.
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+            || (c >= '0' && c <= '9'))
+            result += c;
+
+        // If the scanned character is an
+        // ‘(‘, push it to the stack.
+        else if (c == '(')
+            st.push('(');
+
+        // If the scanned character is an ‘)’,
+        // pop and add to output string from the stack
+        // until an ‘(‘ is encountered.
+        else if (c == ')') {
+            while (st.top() != '(') {
+                result += st.top();
+                st.pop();
+            }
+            st.pop();
+        }
+
+        // If an operator is scanned
+        else {
+            while (!st.empty()
+                && prec(s[i]) <= prec(st.top())) {
+                result += st.top();
+                st.pop();
+            }
+            st.push(c);
+        }
+    }
+
+    // Pop all the remaining elements from the stack
+    while (!st.empty()) {
+        result += st.top();
+        st.pop();
+    }
+
+    return result;
+}
+ // end-Infix2Postfix
+
+///----------------------------------------------------------------------------------
+/// Given a string in post-fix notation, evaluates it and returns the result
+/// 
+int EvaluatePostfixExpression(string& s) {
+	return 0;
+} // end-EvaluatePostfixExpression
